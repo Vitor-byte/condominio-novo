193bc1d4d52ed82707dde0fa8aaf317c070de0d8
Vitor-byte <vitor.cost77@gamil.com>
2022-10-23 20:26:46 -0300

    Alteração de conexão com o banco

--- backend/src/conexao-banco/postgres.ts
+++ backend/src/conexao-banco/postgres.ts
@@ -1,9 +1,13 @@
 import dotenv from "dotenv"
 dotenv.config();
+const isProduction = process.env.NODE_ENV === 'production'
+
 const { Client } = require("pg");
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+
 const client = new Client({
-  connectionString,
+  connectionString: isProduction ? process.env.DATABASE_URL: connectionString
   });
 client.connect();
 
