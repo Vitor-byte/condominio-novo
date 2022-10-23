c247b9721bcd0c191af2f9ef9447a2c4bbbe9707
Vitor-byte <vitor.cost77@gamil.com>
2022-10-23 18:11:35 -0300

    Correção de erros

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,6 @@
-import dotenv from "dotenv"
+require('dotenv').config()
 import express from "express";
 import {router} from "./rotas/rotas";
-
-dotenv.config();
 
 var cors = require('cors')
 const app = express();
